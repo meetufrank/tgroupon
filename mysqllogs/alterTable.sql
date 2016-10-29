125c46ae765949cf5b9a9a2b89e70b4d15c98cce
wangjin <972270516@qq.com>
2016-10-29 17:33:41 +0800

    日志更新

--- mysqllogs/alterTable.sql
+++ mysqllogs/alterTable.sql
@@ -4,3 +4,23 @@ ALTER TABLE `ecs_goods`
 
 ADD COLUMN `arter_id`  smallint(5) NOT NULL AFTER `fencheng`,
 ADD COLUMN `father_id`  smallint(5) NOT NULL AFTER `arter_id`;
+
+
+
+/*#2016/10/29 王晋  创建线下店签约表
+*/
+
+CREATE TABLE `ecs_line_shop` (
+  `ls_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '//编号',
+  `ls_goodsid` int(11) NOT NULL COMMENT '//商品id',
+  `ls_userid` int(11) NOT NULL COMMENT '//线下店id',
+  `ls_ratio` double(4,2) NOT NULL COMMENT '//分成比例',
+  `ls_addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '//签约时间',
+  PRIMARY KEY (`ls_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='//线下店签约表';
+
+/*#2016/10/29 王晋  在ecs_order_info 添加字段线下店id
+*/
+
+ALTER TABLE `ecs_order_info`
+ADD COLUMN `lineshop_id`  smallint(8) NOT NULL COMMENT '//线下店id' AFTER `fencheng`;
