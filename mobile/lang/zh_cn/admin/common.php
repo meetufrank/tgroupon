ea5630b3de39b8f69c31cc35d829a986b75ae3d5
汪江 <1634259995@qq.com>
2017-02-20 14:12:10 +0800

    a

--- mobile/lang/zh_cn/admin/common.php
+++ mobile/lang/zh_cn/admin/common.php
@@ -244,7 +244,7 @@ $_LANG['05_user_rank_list'] = '会员等级';
 $_LANG['06_list_integrate'] = '会员整合';
 $_LANG['09_user_account'] = '充值和提现申请';
 $_LANG['10_user_account_manage'] = '资金管理';
-$_LANG['line_shop'] = '线下店签约管理';
+// $_LANG['line_shop'] = '线下店签约管理';
 $_LANG['line_shop_add'] = '新增线下店签约';
 
 /* 权限管理 */
