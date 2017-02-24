1cc863b8c1a87551073bfc1cb12ec392586f1d88
frank <ql.zhang@meetuuu.com>
2017-02-24 15:00:17 +0800

    1

--- mobile/set_content.php
+++ mobile/set_content.php
@@ -3,8 +3,11 @@
  * @Author: anchen
  * @Date:   2017-02-21 15:47:13
  * @Last Modified by:   anchen
- * @Last Modified time: 2017-02-21 17:16:34
+ * @Last Modified time: 2017-02-24 14:22:23
  */
+
+define('IN_ECTOUCH', true);
+if($_REQUEST['act']=='goods'){
 
 
 if($_POST['type']){
@@ -19,5 +22,15 @@ if($_POST['type']){
                   setcookie('typeid',$typeid);
              }
             echo json_encode(1);
-             exit;
+
+}
+ exit;
+}elseif($_REQUEST['act']=='ysj'){
+
+    if($_POST['content']){
+        setcookie('ysj_content',$_POST['content']);
+        setcookie('is_send',1);
+        echo json_encode(1);
+    }
+exit;
 }
