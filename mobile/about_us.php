bb58e51decdd2bddf25c0b412406760683db4bfc
frank <ql.zhang@meetuuu.com>
2017-03-02 15:35:36 +0800

    1

--- mobile/about_us.php
+++ mobile/about_us.php
@@ -3,7 +3,7 @@
  * @Author: anchen
  * @Date:   2017-03-02 10:14:48
  * @Last Modified by:   anchen
- * @Last Modified time: 2017-03-02 13:08:13
+ * @Last Modified time: 2017-03-02 15:09:38
  */
 define('IN_ECTOUCH', true);
 
@@ -27,9 +27,36 @@ $smarty->display('about_us.dwt');
 exit;
 }elseif($_REQUEST['act']=='add_hezuo'){
 
-$username=$_POST['UserName'];
-$username=$_POST['UserName'];
-$username=$_POST['UserName'];
-print_r($content);exit;
+ if($_SESSION['user_id'] <= 0){
+       ajax_please_in();
+     }
+    $userid=$_SESSION['user_id'];
+
+$username=$_POST['UserName']?$_POST['UserName']:'';
+$tel=$_POST['UserTel']?$_POST['UserTel']:'';
+$email=$_POST['UserMail']?$_POST['UserMail']:'';
+$weixin=$_POST['UserWeixin']?$_POST['UserWeixin']:'';
+$kjz_name=$_POST['SpaceName']?$_POST['SpaceName']:'';
+$kjz_add=$_POST['SpaceAddr']?$_POST['SpaceAddr']:'';
+$hz_content=$_POST['TextSay']?$_POST['TextSay']:'';
+if($_REQUEST['type']){
+    $type=intval($_REQUEST['type']);
+}else{
+    $type=1;
+}
+
+ $sql="insert into ecs_shenqing
+(sq_userid,sq_username,sq_tel,sq_email,sq_wxname,sq_kjname,sq_kjaddress,sq_text,sq_type) values
+ ($userid,'$username','$tel','$email','$weixin','$kjz_name','$kjz_add','$hz_content',$type)
+ ";
+ $result=$GLOBALS['db']->query($sql);
+ if($result){
+    $data['msg']='true';
+    echo json_encode($data);
+ }else{
+    $data['msg']='false';
+    echo json_encode($data);
+ }
+exit;
 
 }
