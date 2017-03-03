03a9d2f3bad59b750d3a72e5e9b8bdeacf893ed9
汪江 <1634259995@qq.com>
2017-03-03 14:45:18 +0800

    aa

---
+++ mobile/my_userxiaoxi.php
@@ -0,0 +1,20 @@
+<?php
+
+/*个人中心 --- 我的消息*/
+
+define('IN_ECTOUCH', true);
+
+require(dirname(__FILE__) . '/include/init.php');
+include('head.php');
+
+
+
+
+
+
+$smarty->display('my_userxiaoxi.dwt');
+
+
+
+
+?>
